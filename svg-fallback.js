e004b5611071668d9c2947cb2637e0d19f7a997e
The Frontend <alex@thefrontend.uk>
2015-10-24 22:46:26 +0100

    add svg swap function

--- svg-fallback.js
+++ svg-fallback.js
@@ -2,33 +2,43 @@
 // SVG Swap
 // ==============================================
 
-
+// Check to see if browser supports SVG
 function supportsSvg() {
   return document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1");
 }
 
+// Swap out .svg for .png or .jpg depending on class
+(function() {
+  if(supportsSvg()) {
 
-function svgSwap() {
-  if(!supportsSvg()) {
-    $('.svg-swap').each(function() {
-      var el = $(this),
-          src = el.attr('src'),
-          fallbackFormat = el.data('format');
-          newSrc = src.replace('svg',fallbackFormat),
-          fallbackImg = el.data('fallback');
+    var svgEls = document.getElementsByClassName('svg-fallback'), pngEls = [], jpgEls = [];
 
-      if( typeof fallbackImg !== 'undefined' ) {
-        var fallback = fallbackImg;
-        el.attr('src', fallback);
-      }
-      else {
-        el.attr('src', newSrc);
+    for(var i = 0; i < svgEls.length; i++) {
+
+      var classes = svgEls[i].className;
+
+      if( classes.indexOf('png') !== -1 ) {
+        pngEls.push(svgEls[i]);
       }
 
-    });
+      if( classes.indexOf('jpg') !== -1 ) {
+        jpgEls.push(svgEls[i]);
+      }
+    }
+
+    if(pngEls.length) {
+      for(var p = 0; p < pngEls.length; p++) {
+        var pngEl = pngEls[p], pngSrc = pngEl.src; pngSrc = pngSrc.replace('.svg','.png');
+        pngEl.src = pngSrc;
+      }
+    }
+
+    if(jpgEls.length) {
+      for(var j = 0; j < jpgEls.length; j++) {
+        var jpgEl = jpgEls[j], jpgSrc = jpgEl.src; jpgSrc = jpgSrc.replace('.svg','.jpg');
+        jpgEl.src = jpgSrc;
+      }
+    }
+
   }
-  return;
-}
-
-$(svgSwap());
-*/
+})();
